0a461fcecc3f9beed45a6f5844fbeae7dabd13f8
MykhayloGor <ursa.mykh@gmail.com>
2024-12-07 21:03:36 +0200

    task-t

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 //менше з чисел a, b
 //Потрібно додати перевірку, що функція отримує числа
 
-function min(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    return "Мають бути передані числа";
-  }
-  return a > b ? b : a;
-  //   return Math.min(a, b);
-}
-console.log(min(3, "5"));
+// function min(a, b) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     return "Мають бути передані числа";
+//   }
+//   return a > b ? b : a;
+//   //   return Math.min(a, b);
+// }
+// console.log(min(3, "5"));
 
 // Task Зарано
 //Напиши функцію findLongestWord(string)
@@ -25,18 +25,40 @@ console.log(min(3, "5"));
 //У сторіччі на протилежний
 //Наприклад 'JavaScript' повинен повернути 'jAVAsCRIPT'
 
-function makeInvetedString(str) {
-  let newStr = "";
+// function makeInvetedString(str) {
+//   let newStr = "";
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == str[i].toLowerCase()) {
-      newStr += str[i].toUpperCase();
-    } else {
-      newStr += str[i].toLowerCase();
-    }
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] == str[i].toLowerCase()) {
+//       newStr += str[i].toUpperCase();
+//     } else {
+//       newStr += str[i].toLowerCase();
+//     }
+//   }
+
+//   return newStr;
+// }
+
+// console.log(makeInvetedString("jAVAsCRIPT"));
+
+// / У змінній min лежить число від 0 до 59.
+// Визначте, в яку чверть години потрапляє
+// це число(у першу, другу, третю чи четверту).
+
+function timeInterval(min) {
+  if (min >= 0 && min <=15) {
+    return "first part";
   }
-
-  return newStr;
+  if (min >=16 && min <= 30) {
+    return "second part";
+  }
+  if (min >= 31 && min <= 45) {
+    return "third part";
+  } if (min >= 46 && min <=59) {
+    return "forth part";
+  }
+  return 'invalid value';
 }
 
-console.log(makeInvetedString("jAVAsCRIPT"));
+
+console.log(timeInterval(15));
