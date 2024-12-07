09249efd4565bafa39b6ee79be9dca16bdebd99d
Andrii Semenenko <oxonomy14@gmail.com>
2024-12-08 01:04:55 +0700

    task 2

--- js/main.js
+++ js/main.js
@@ -10,3 +10,33 @@ function min(a, b) {
   //   return Math.min(a, b);
 }
 console.log(min(3, "5"));
+
+// Task Зарано
+//Напиши функцію findLongestWord(string)
+//яка приймає довільний рядок
+//що складається лише з розділених слів
+//пробілом (параметр string)
+//і повертає найдовше слово у цьому рядку
+
+// const string = "Nunc sed turpis a felis in nunc fringilla"
+
+// Task 2
+//Напишіть функцію makeInvetedString(), яка замінює регістр кожного символу
+//У сторіччі на протилежний
+//Наприклад 'JavaScript' повинен повернути 'jAVAsCRIPT'
+
+function makeInvetedString(str) {
+  let newStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i].toLowerCase()) {
+      newStr += str[i].toUpperCase();
+    } else {
+      newStr += str[i].toLowerCase();
+    }
+  }
+
+  return newStr;
+}
+
+console.log(makeInvetedString("jAVAsCRIPT"));
