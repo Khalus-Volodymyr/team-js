283b718c7e7f86d148931df91cb80c53fb29b441
Andrii Semenenko <oxonomy14@gmail.com>
2024-12-22 03:54:59 +0700

    js Maryna

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -54,7 +54,7 @@ function findSmallerNumber(numbers) {
 //Рядок з назвою каменю.
 //Функція повертає загальну вартість каменів
 //з ​​таким ім'ям, ціною та кількістю з об'єкта
-
+/*
 const stones = [
   { name: "Смарагд", price: 1300, quantity: 4 },
   { name: "Діамант", price: 2700, quantity: 6 },
@@ -70,4 +70,24 @@ function calcTotalPrice(stones, stonesName) {
   }
 }
 
-console.log(calcTotalPrice(stones, "Щебінь"));
+console.log(calcTotalPrice(stones, "Щебінь")); */
+
+// Угруповання об'єктів студентів по курсам
+// Створіть масив об'єктів, що являють собою різних студентів. Кожен об'єкт повинен містити властивості ім'я, вік та курс.
+// Використовуйте цикл for...of, щоб згрупувати студентів за курсами.
+// Результатом буде об'єкт, де ключами будуть назви курсів, а значення – масиви об'єктів студентів, що належать до відповідного курсу.
+// Очікуванний результат:
+// const groupedStudents = {
+//   Java: [
+//     { name: "Алиса", age: 20, course: "Java" },
+//     { name: "Карл", age: 21, course: "Java" },
+//   ],
+//   Python: [{ name: "Боб", age: 22, course: "Python" }],
+//   JavaScript: [{ name: "Джон", age: 23, course: "JavaScript" }],
+// };
+const students = [
+  { name: "Алиса", age: 20, course: "Java" },
+  { name: "Боб", age: 22, course: "Python" },
+  { name: "Карл", age: 21, course: "Java" },
+  { name: "Джон", age: 23, course: "JavaScript" },
+];
