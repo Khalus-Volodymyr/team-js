a45204e44641bd3f52b418ee5430b24f3e16b2db
PushEax <pusheax.vk@gmail.com>
2024-12-21 19:32:26 +0200

    add function calculateAverage

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -23,4 +23,30 @@ function findSmallerNumber(numbers) {
   return Math.min(...numbers);
 }
 
-console.log(findSmallerNumber(numbers));
+// console.log(findSmallerNumber(numbers));
+
+
+/*
+Напишіть функцію caclculateAverage()
+яка приймає довільну кількість
+аргументів і повертає їхнє середнє значення.
+Додати перевірку, що аргументи це числа.
+
+*/
+
+const arr = [2, 6, 24, 7, 35, 24, "hello", 9, "world"];
+
+function caclculateAverage(...args) {
+  let sum = 0;
+  let count = 0;
+  for (const num of args) {
+    if (typeof num === "number") {
+      sum += num;
+      count += 1;
+    }
+  }
+  return sum / count;
+}
+
+console.log(caclculateAverage(...arr));
+
